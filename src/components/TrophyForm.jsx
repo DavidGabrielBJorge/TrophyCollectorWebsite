60c1d42a966eab35b26b89e69c7d1b39914172a3
DavidGabrielBJorge <davidgbjorge@gmail.com>
2025-02-04 10:37:07 -0300

    add: creating form state management

--- src/components/TrophyForm.jsx
+++ src/components/TrophyForm.jsx
@@ -1,19 +1,41 @@
 import React from 'react'
+import { useState } from 'react'
 
 const TrophyForm = () => {
+    const [name, setName] = useState("");
+    const [text, setText] = useState("");
+    const [category, setCategory] = useState("");
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        //Verify if every camp is occupy
+        if(!name || !text ||  !category){
+            console.log("onde of the fields are empty ")
+            return;
+        }
+        else{
+            console.log(name, text, category);
+        }
+        setName("");
+        setText("");
+        setCategory("");
+    }
   return (
     <div className='trophy-form'>
         <h2>Criar tarefa</h2>
-        <form>
-            <input type="text" placeholder="Enter the name of the game"></input>
-            <input type="text" placeholder="Enter the details of the game"></input>
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Enter the name of the game" value={name} onChange={(e) => setName(e.target.value)}></input>
+            <input type="text" placeholder="Enter the details of the game" value={text} onChange={(e) => setText(e.target.value)}></input>
+
+            <select value={category} onChange={(e) => setCategory(e.target.value)}>
+                <option value="">Select a category</option>
+                <option value="Category 1">Category 1</option>
+                <option value="Category 2">Category 2</option>
+                <option value="Category 3">Category 3</option>
+            </select>
+            <button type='submit'>Create Task</button>
         </form>
-        <select>
-            <option value="Category 1">Category 1</option>
-            <option value="Category 2">Category 2</option>
-            <option value="Category 3">Category 3</option>
-        </select>
-        <button type='submit'>Create Task</button>
+        
 
     </div>
   )
